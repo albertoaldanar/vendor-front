909a98ac1f4aca928df1ff895b0461e4db58c828
Alberto Aldana <albertoaldanar@hotmail.com>
2018-10-09 17:05:23 -0600

    projection working

--- src/components/reusable/chartPrediction.js
+++ src/components/reusable/chartPrediction.js
@@ -3,48 +3,53 @@ import {Line} from "react-chartjs-2";
 
 //Hacer las intancias para que puedan consumirse en el ftin
 class ChartPrediction extends Component{
+
   constructor(props){
     super(props);
     this.state = {
-      data:{
-        labels: ["Semana 1", "Semana 2", "Semena 3", "Semana 4"],
-        datasets: [
-          {
-            label: "Meta",
-            data: [
-              184234,
-              213333,
-              224233,
-              254233,
-            ],
-            fill: false,
-            borderColor: "red",
-            borderDash: [1,1],
-            borderWidth: 3
-          },
-          {
-          label: "Ventas",
-            data: [
-              165555,
-              179549,
-              225473,
-              244834,
-            ],
-            fill: false,
-            borderColor: "rgba(75,192,192,0.6)",
-            backgroundColor:"rgba(75,192,192,0.6)",
-            borderWidth: 3
-          }
-        ]
-      }
+      projection: []
     }
   }
 
+  componentWillMount(){
+    fetch("http://localhost:3000/api/projection")
+    .then(response => response.json())
+    .then( res =>  {
+      this.setState({projection: res.projection})
+    })
+  }
+
   render(){
+    const {projection} = this.state;
     return(
       <div>
         <Line
-          data = {this.state.data}
+          data = {{
+            labels: ["Semana 1", "Semana 2", "Semena 3", "Semana 4"],
+            datasets: [
+              {
+                label: "Meta",
+                data: projection,
+                fill: false,
+                borderColor: "red",
+                borderDash: [1,1],
+                borderWidth: 3
+              },
+              {
+              label: "Ventas",
+                data: [
+                  165555,
+                  179549,
+                  225473,
+                  244834,
+                ],
+                fill: false,
+                borderColor: "rgba(75,192,192,0.6)",
+                backgroundColor:"rgba(75,192,192,0.6)",
+                borderWidth: 3
+              }
+            ]
+          }}
           width= {240}
           height = {240}
           options = {{
