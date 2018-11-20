6142125b1860366f23c3df87af6ae6894ade9271
Alberto Aldana <albertoaldanar@hotmail.com>
2018-11-20 12:50:59 -0700

    sales fetching goos

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,7 +1,27 @@
 import React, {Component} from "react";
 
 class Settings extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      unauthorizedSales: []
+    }
+  }
+
+  componentWillMount(){
+    return fetch("http://localhost:3000/api/authorization")
+      .then(response => response.json())
+        .then(res => {
+          this.setState({
+            unauthorizedSales: res.unauthorized
+          })
+        })
+      .catch(e => console.log(e))
+  };
+
   render(){
+    console.log(this.state.unauthorizedSales)
     return(
       <div>
         <p className = "center">Settings</p>
