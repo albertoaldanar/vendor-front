e501a222335b8ef06020729f43e00daad8673d81
Alberto Aldana Ríos <albertoaldanarios@MacBook-Pro-de-Alberto.local>
2019-09-14 23:39:14 -0600

    Text error

--- src/components/settings.js
+++ src/components/settings.js
@@ -66,22 +66,6 @@ class Settings extends Component{
   }
 
 
-    renderData(){
-        console.log(this.state.ingresosThisMonth);
-
-        if(this.state.ingresosThisMonth.length > 0 ){
-            return this.state.ingresosThisMonth.map(x=> {
-                return(
-                    <Text style = {styles.movieTitle}>{x.importe}</Text>
-                )
-            })
-        } else {
-            return <Text>A mamarla</Text>
-        }
-
-    }
-
-
   showData(){
     const {ingresos} = this.state;
     const months = { 1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre" }
